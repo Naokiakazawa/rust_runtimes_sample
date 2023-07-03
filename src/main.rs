1759a09cb76ed461d8f616eb0e55f8c31e26e599
Naokiakazawa <naoki9akazawa@gmail.com>
2023-07-03 18:23:07 +0900

    add onnxruntime

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
-}
+use onnxruntime::{environment::Environment, ndarray::Array, GraphOptimizationLevel, LoggingLevel};
+use std::env::var;
+use tracing::Level;
+use tracing_subscriber::FmtSubscriber;
+
+type Error = Box<dyn std::error::Error>;
+
+fn run() -> Result<(), Error> {}
+
+fn main() {}
