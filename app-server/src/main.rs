34e36c7c5c15453ad0a11689cb47d04740203479
Naokiakazawa <naoki9akazawa@gmail.com>
2024-01-20 19:03:55 +0900

    fix docker

--- app-server/src/main.rs
+++ app-server/src/main.rs
@@ -52,7 +52,7 @@ async fn main() -> std::io::Result<()> {
                     .to(graphiql),
             )
     })
-    .bind("127.0.0.1:8000")?
+    .bind("0.0.0.0:8000")?
     .run()
     .await
 }
